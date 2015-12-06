179aca83dc3f9219f3f93298486b3d26991b7a4f
xomachine <xomachiner@gmail.com>
2015-12-06 23:47:37 +0300

    Applications menu import

--- menu.lua
+++ menu.lua
@@ -1,21 +1,82 @@
 local module = {}
 local awful = require("awful")
 local beautiful = require("beautiful")
--- local menugen = require("menubar.menu_gen")
+local menugen = require("menubar.menu_gen")
+local mutil = require("menubar.utils")
+local notify = require("naughty").notify
 
-function create_launcher()
+module.create_launcher = function()
   --local terminal = "termite"
+  local desktops = mutil.parse_dir("/usr/share/applications")
+  local maincategories = {AudioVideo = {"Мультимедиа", {}},
+                          Audio = {"Звук", {}},
+                          Video = {"Видео", {}},
+                          Developement = {"Разработка", {}},
+                          Education = {"Образование", {}},
+                          Game = {"Игры", {}},
+                          Graphics = {"Графика", {}},
+                          Network = {"Интернет", {}},
+                          Office = {"Офис", {}},
+                          Science = {"Наука", {}},
+                          Settings = {"Настройки", {}},
+                          System = {"Система", {}},
+                          Utility = {"Утилиты", {}},
+                          Others = {"Другие", {}}
+                  }
+
+  appmenu = {} --menugen.generate()
+  for i, desktop in ipairs(desktops) do
+    local category = "Others"
+    local appname = "Noname"
+    local exec = ""
+    local icon = nil
+    local dir = "."
+    if desktop.Hidden == true or desktop.NoDisplay == true then
+      goto continue
+    end
+    if desktop.Path then
+      dir = desktop.Path
+    end
+
+    if desktop.Exec then
+      exec = "sh -c \"cd '"..dir.."'; "..desktop.Exec.."\""
+    end
+    if desktop.Terminal == true then
+      exec = terminal.." -e "..exec
+    end
+    if desktop.Icon then
+      icon = mutil.lookup_icon(desktop.Icon)
+    end
+    if desktop.categories then
+      for k, v in pairs(desktop.categories) do
+        if maincategories[v] then
+          category = v
+        end
+      end
+    end
+    if desktop.Name then
+      appname = desktop.Name
+    elseif desktop.GenericName then
+      appname = desktop.GenericName
+    end
+    --notify({text = appname..": ".. exec, timeout = 0})
+    table.insert(maincategories[category][2], {appname, exec, icon})
+    ::continue::
+  end
+  for i, submenu in pairs(maincategories) do
+    table.insert(appmenu, submenu)
+  end
   myawesomemenu = {
      { "Помощь", terminal .. " -e man awesome" },
-     { "Редактировать конфигурацию", "gvim " .. awesome.conffile },
-     { "Перезапуск awesome", awesome.restart },
+     { "Конфигурация", "gvim " .. awesome.conffile },
+     { "Перезапуск", awesome.restart },
      { "Выйти", awesome.quit }
   }
   -- Favorites menu
   favoritesmenu = {
      { "Обозреватель", "firefox" },
      { "Терминал", terminal },
-     { "Файловый менеджер", "pcmanfm" }
+     { "Файлы", "pcmanfm" }
   }
   systemmenu = {
      { "Перезагрузка", "reboot" },
@@ -23,9 +84,9 @@ function create_launcher()
   }
 
   mymainmenu = awful.menu({ items = { { "Избранное", favoritesmenu },
+                                      { "Applications", appmenu },
                                       { "awesome", myawesomemenu, beautiful.awesome_icon },
                                       { "Система", systemmenu },
-                                      { "Открыть терминал", terminal }
                                     }
                           })
 
@@ -34,5 +95,4 @@ function create_launcher()
   return mylauncher
 end
 
-module.create_launcher = create_launcher
 return module
