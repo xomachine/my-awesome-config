3a19d2e3bf5c7dfe24725f8d042404ab9cb9ff74
xomachine <xomachiner@gmail.com>
2015-12-15 17:29:55 +0300

    Bypass of inserting paths from my system to sources

--- wallpaper.lua
+++ wallpaper.lua
@@ -1,11 +1,18 @@
 local beautiful = require("beautiful")
 local gears = require("gears")
 
+local wallpaper = beautiful.wallpaper
+
+local custompath = io.open(".config/awesome/wallpaper.path")
+if custompath ~= nil then
+    wallpaper = custompath:read()
+    custompath:close()
+end
 -- {{{ Wallpaper
 if beautiful.wallpaper then
     for s = 1, screen.count() do
         gears.wallpaper.maximized(
-        "/home/xomachine/Изображения/Arts/WallPapers/gunnm_lo_yoko_child.jpg", s, false)
+        wallpaper, s, false)
     end
 end
 -- }}}
