7f90174fc84b02f9d229333f32441e687011814a
xomachine <xomachiner@gmail.com>
2017-04-12 18:51:49 +0300

    Tasklist widget

---
+++ widgets/tasklist.lua
@@ -0,0 +1,47 @@
+local awful = require("awful")
+local gears = require("gears")
+
+local function client_menu_toggle_fn()
+    local instance = nil
+
+    return function ()
+        if instance and instance.wibox.visible then
+            instance:hide()
+            instance = nil
+        else
+            instance = awful.menu.clients({ theme = { width = 250 } })
+        end
+    end
+end
+
+local tasklist_buttons = gears.table.join(
+                     awful.button({ }, 1, function (c)
+                                              if c == client.focus then
+                                                  c.minimized = true
+                                              else
+                                                  -- Without this, the following
+                                                  -- :isvisible() makes no sense
+                                                  c.minimized = false
+                                                  if not c:isvisible() and c.first_tag then
+                                                      c.first_tag:view_only()
+                                                  end
+                                                  -- This will also un-minimize
+                                                  -- the client, if needed
+                                                  client.focus = c
+                                                  c:raise()
+                                              end
+                                          end),
+                     awful.button({ }, 3, client_menu_toggle_fn()),
+                     awful.button({ }, 4, function ()
+                                              awful.client.focus.byidx(1)
+                                          end),
+                     awful.button({ }, 5, function ()
+                                              awful.client.focus.byidx(-1)
+                                          end))
+
+
+return function(screen)
+  screen.tasklist = awful.widget.tasklist(screen, awful.widget.tasklist.filter.currenttags, tasklist_buttons)
+  return screen.tasklist
+end
+
