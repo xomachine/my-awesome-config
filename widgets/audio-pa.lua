9395ced04237a3c4e3fac61a696fe314ac07d01a
xomachine <xomachiner@gmail.com>
2020-08-16 18:50:38 +0300

    Makes audio widget on second screen syncronized with main screen

--- widgets/audio-pa.lua
+++ widgets/audio-pa.lua
@@ -4,7 +4,12 @@ local notify = require("naughty").notify
 local widget = require("wibox.widget")
 local unpack = unpack or table.unpack
 
-local AudioWidget = {}
+local AudioWidget = {
+  master_sink = "@DEFAULT_SINK@",
+  master_source = "@DEFAULT_SOURCE@",
+  msgid = nil,
+  widgets = {}
+}
 
 local function parse_volume(volstr)
   local result = {}
@@ -82,72 +87,84 @@ local function parse_pactl_list(on_done)
                                  {stdout = cb, output_done = donecb})
 end
 
-function AudioWidget:update_volume()
-  parse_pactl_list(self.volume_callback)
+function AudioWidget.redraw_all(pactl_list)
+  local mastersink = pactl_list.Sink[AudioWidget.master_sink]
+  if not mastersink then return end
+  local vol = parse_volume(mastersink.Volume)
+  local mute = mastersink.Mute
+  local _,_,bal = string.find(mastersink.Volume, "balance (%d+%.%d+)")
+  if mute == "yes" then
+    AudioWidget.displayState("Звук", "off")
+  elseif bal == "0.00" then
+    AudioWidget.displayState("Громкость", tostring(vol[1]))
+  else
+    AudioWidget.displayState("Громкость", tostring(vol[1]).."|"..tostring(vol[2]))
+  end
 end
 
-function AudioWidget:change_volume(value)
-  local cb = function() self:update_volume() end
-  awful.spawn.easy_async("pactl set-sink-volume "..self.master_sink..
-                         " "..value, cb)
+function AudioWidget.update_volume()
+  parse_pactl_list(AudioWidget.redraw_all)
 end
 
-function AudioWidget:toggle_mute()
-  local cb = function() self:update_volume() end
-  awful.spawn.easy_async("pactl set-sink-mute "..self.master_sink..
-                         " toggle", cb)
+function AudioWidget.update_defaults(pactl_list)
+  get_default_sink_source(pactl_list, function (sink, source)
+    AudioWidget.master_sink = sink
+    AudioWidget.master_source = source
+    AudioWidget.redraw_all(pactl_list)
+  end)
 end
 
-function AudioWidget:displayState(message, state)
-  local n = notify({text=message .. ": " .. state, replaces_id=self.msgid})
-  self.msgid = n.id
+function AudioWidget.change_volume(value)
+  awful.spawn.easy_async("pactl set-sink-volume "..AudioWidget.master_sink..
+                         " "..value, AudioWidget.update_volume)
+end
+
+function AudioWidget.toggle_mute()
+  awful.spawn.easy_async("pactl set-sink-mute "..AudioWidget.master_sink..
+                         " toggle", AudioWidget.update_volume)
+end
+
+function AudioWidget.displayState(message, state)
+  local n = notify({text=message .. ": " .. state, replaces_id=AudioWidget.msgid})
+  AudioWidget.msgid = n.id
+  for widget, _ in pairs(AudioWidget.widgets) do
+    widget:set_state(state)
+  end
+end
+
+function AudioWidget:set_state(state)
   self.widget:set_text("["..state.."]")
 end
 
 function AudioWidget.new()
   local av = {}
   av.widget = widget.textbox()
-  av.master_sink = "@DEFAULT_SINK@"
-  av.master_source = "@DEFAULT_SOURCE@"
   av.msgid = nil
-  av.volume_callback = function(pactl_list)
-    local mastersink = pactl_list.Sink[av.master_sink]
-    local vol = parse_volume(mastersink.Volume)
-    local mute = mastersink.Mute
-    local _,_,bal = string.find(mastersink.Volume, "balance (%d+%.%d+)")
-    if mute == "yes" then av:displayState("Звук", "off")
-    elseif bal == "0.00" then av:displayState("Громкость", tostring(vol[1]))
-    else av:displayState("Громкость", tostring(vol[1]).."|"..tostring(vol[2]))
-    end
-  end
   local buts = {
     awful.button({}, 1, function() awful.spawn.spawn("pavucontrol") end),
-    awful.button({}, 2, function() av:toggle_mute() end),
-    awful.button({}, 4, function() av:change_volume("+3%") end),
-    awful.button({}, 5, function() av:change_volume("-3%") end),
+    awful.button({}, 2, function() AudioWidget.toggle_mute() end),
+    awful.button({}, 4, function() AudioWidget.change_volume("+3%") end),
+    awful.button({}, 5, function() AudioWidget.change_volume("-3%") end),
   }
   av.widget:buttons(awful.util.table.join(unpack(buts)))
-  parse_pactl_list(function(pactl_list)
-    local default_setter = function(sink, source)
-      av.master_sink = sink
-      av.master_source = source
-      av.volume_callback(pactl_list)
-    end
-    get_default_sink_source(pactl_list, default_setter)
-  end)
+  AudioWidget.update_volume()
+  AudioWidget.widgets[av] = true
   return setmetatable(av, {__index=AudioWidget})
 end
+
+parse_pactl_list(AudioWidget.update_defaults)
+
 return {
   factory = function(screen)
     local aw = AudioWidget.new()
     return aw
   end,
   bindings = gears.table.join(
-      awful.key({}, "XF86AudioMute", function() aw:toggle_mute() end),
+      awful.key({}, "XF86AudioMute", function() AudioWidget.toggle_mute() end),
       awful.key({}, "XF86AudioRaiseVolume",
-                function() aw:change_volume("+3%") end),
+                function() AudioWidget.change_volume("+3%") end),
       awful.key({}, "XF86AudioLowerVolume",
-                function() aw:change_volume("-3%") end)
+                function() AudioWidget.change_volume("-3%") end)
   )
 }
 
